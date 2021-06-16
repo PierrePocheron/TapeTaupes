4b6781f70cfc686a7fc6da2921f2b84ea43447c3
PierrePocheron <pierre.pocheron@gmail.com>
2021-06-16 16:38:08 +0200

    Many update

--- sketch_jun14a.ino
+++ sketch_jun14a.ino
@@ -5,26 +5,32 @@ int partieTime = 60;
 
 const int size = 9;
 
+const int sizeCircle = 5;
+
+int circleCenterX = (gb.display.width()/2) - (sizeCircle / 2);
+int circleCenterY = (gb.display.height()/2) - (sizeCircle / 2);
+
+
+struct Point {
+    uint posX;
+    uint posY;
+};
+
+//Dimensions 80 x 64
+Point tabHole[size] = {{circleCenterX - 20, circleCenterY - 20}, {circleCenterX, circleCenterY - 20}, {circleCenterX + 20, circleCenterY - 20},
+                       {circleCenterX - 20, circleCenterY}, {circleCenterX, circleCenterY}, {circleCenterX + 20, circleCenterY},
+                       {circleCenterX - 20, circleCenterY + 20}, {circleCenterX, circleCenterY + 20}, {circleCenterX + 20, circleCenterY + 20}};
+
 class Personnage
 {
     private:
         // Personnage - Taille
         int personnageSizeX = 2;
-        int personnageSizeY = 6;
+        int personnageSizeY = 2;
 
-        // Personnage - Vitesse
-        int personnageSpeedX = 2;
-        int personnageSpeedY = 2;
 
-        // Personnage - Vitesse
-        int personnagePositionX = gb.display.width()/2;
-        int personnagePositionY = gb.display.height()/2;
-
-        // Personnage - Déplacement max
-        int personnageMaxPositionXDroite = gb.display.width() - personnageSizeX;
-        int personnageMaxPositionXGauche = 0;
-        int personnageMaxPositionYHaut = 0;
-        int personnageMaxPositionYBas = gb.display.height() - personnageSizeY;
+        // Personnage - Position
+        int personnagePosition = 5;
 
         // Personnage - Score
         int personnageScore = 0;
@@ -35,46 +41,37 @@ class Personnage
         };
 
         void update() {
-          // Utilisateur - onClic - Button Left
-          if (gb.buttons.pressed(BUTTON_LEFT)) 
-          {
-            if(personnagePositionX > personnageMaxPositionXGauche)
-            {
-              personnagePositionX -= personnageSpeedX;
-            }
-          }
-
-          // Utilisateur - onClic - Button Right
-          if (gb.buttons.pressed(BUTTON_RIGHT)) 
-          {
-            if(personnagePositionX < personnageMaxPositionXDroite)
-            {
-              personnagePositionX += personnageSpeedX;
-            }
-          }
-
-          // Utilisateur - onClic - Button Up
-          if (gb.buttons.pressed(BUTTON_UP)) 
-          {
-            if(personnagePositionY > personnageMaxPositionYHaut)
-            {
-              personnagePositionY -= personnageSpeedY;
-            }
-          }
-
-          // Utilisateur - onClic - Button Down
-          if (gb.buttons.pressed(BUTTON_DOWN)) 
-          {
-            if(personnagePositionY < personnageMaxPositionYBas)
-            {
-              personnagePositionY += personnageSpeedY;
-            }
-          }
-
           // Affichage Graphique - Le Personnage
-          gb.display.setColor(BLUE);
-          gb.display.fillRect(personnagePositionX, personnagePositionY, personnageSizeX, personnageSizeY);
+        //   gb.display.setColor(BLUE);
+        //   gb.display.fillRect(personnagePositionX, personnagePositionY, personnageSizeX, personnageSizeY);
 
+        }
+
+        void move(Button action) {
+            if(action == BUTTON_UP) {
+                if(personnagePosition > 3)
+                {
+                    personnagePosition - 3;
+                }
+            }
+            if(action == BUTTON_DOWN) {
+                if(personnagePosition < 7)
+                {
+                    personnagePosition + 3;
+                }
+            }
+            if(action == BUTTON_LEFT) {
+                if(personnagePosition > 1)
+                {
+                    personnagePosition - 1;
+                }
+            }
+            if(action == BUTTON_RIGHT) {
+                if(personnagePosition < 9)
+                {
+                    personnagePosition - 1;
+                }
+            }
         }
 };
 
@@ -102,6 +99,10 @@ class Taupe {
             if(deltaInSec > time) {
                 //Si le temps est dépassé alors on l'as fait despawn
             }
+        }
+
+        void render() {
+            //Partie graphique
         }
 
         bool isOut() {
@@ -144,6 +145,23 @@ class Controller {
             } 
         }
 
+        //Faire le render après l'update
+        void render() {
+            for(Point hole : tabHole) {
+                gb.display.setColor(GRAY);
+                gb.display.drawCircle(hole.posX, hole.posY, sizeCircle);
+            }
+
+            Taupe* taupeCurrent;
+            for(uint i = 0; i < size; i++) {
+                taupeCurrent = taupeInGame[i];
+                if(taupeCurrent != nullptr) {
+                    //Il y a bien une taupe présente dans le trou
+                    taupeCurrent->render();
+                }
+            } 
+        }
+
         void spawnMole() {
             uint positionMole;
             bool isGood = false;
@@ -175,4 +193,5 @@ void loop() {
   gb.display.clear();
 
   gameController.update();
+  gameController.render();
 }
